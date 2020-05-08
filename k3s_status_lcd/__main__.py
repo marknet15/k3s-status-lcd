9f37f919a0df48e884688df70b781f5cb5340439
Mark Woolley <mw@marknet15.com>
2020-05-08 14:40:49 +0100

    Lookup actual node & pod count from k8s

---
+++ k3s_status_lcd/__main__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from k3s_status_lcd.status import run
+run()
--- k3s_status_lcd/status.py
+++ k3s_status_lcd/status.py
@@ -5,10 +5,9 @@ Render k3s cluster stats on a LCD display
 
 import os
 import time
-import struct
-import socket
-import fcntl
+import datetime
 import i2c_lcd
+import logging
 
 from kubernetes import client, config
 
@@ -17,36 +16,66 @@ DISPLAY_DELAY = 10
 try:
     config.load_kube_config(os.path.join(os.environ["HOME"], '.kube/config'))
 except Exception as err:
-    print(err)
+    logging.error(f"The following error occured: {err}")
 
 kube_cli = client.CoreV1Api()
 lcd = i2c_lcd.lcd()
 
+def get_nodes():
+    nodes = kube_cli.list_node(watch=False)
+    ready_nodes = 0
+    total_nodes = len(nodes.items)
+
+    for node in nodes.items:
+        for condition in node.status.conditions:
+            if condition.type == "Ready" and condition.status == "True":
+                ready_nodes += 1
+
+    return ( ready_nodes, total_nodes)
+
+def get_all_pods():
+    pods = kube_cli.list_pod_for_all_namespaces(timeout_seconds=10)
+    total_pods = len(pods.items)
+    return total_pods
+
 def print_welcome():
+    date = datetime.date.today()
+    time = datetime.datetime.now().strftime('%H:%M')
+
     lcd.lcd_clear()
     lcd.lcd_display_string('k3s lcd status svc', 1)
     lcd.lcd_display_string('running ^_^', 2)
+    lcd.lcd_display_string(str(date), 3)
+    lcd.lcd_display_string(str(time), 4)
 
 def print_k3s_overview():
+    ready_nodes, total_nodes = get_nodes()
+    total_pods = get_all_pods()
+    nodes_line = f"nodes ready: {ready_nodes}/{total_nodes}"
+    pods_line = f"pods running: {total_pods}"
+
     lcd.lcd_clear()
     lcd.lcd_display_string('k3s status', 1)
-    lcd.lcd_display_string('nodes: 3', 2)
-    lcd.lcd_display_string('pods: 8', 3)
+    lcd.lcd_display_string(nodes_line, 2)
+    lcd.lcd_display_string(pods_line, 3)
 
-if __name__ == "__main__":
-    print("starting service")
+def run():
+    logging.info("starting service")
     try:
         print("printing welcome message")
         print_welcome()
         time.sleep(DISPLAY_DELAY)
-        
+
         while True:
-            print("printing k3s overview")
+            logging.info("printing k3s overview")
             print_k3s_overview()
             time.sleep(DISPLAY_DELAY)
 
-            print("printing welcome message")
+            logging.info("printing welcome message")
             print_welcome()
             time.sleep(DISPLAY_DELAY)
     except KeyboardInterrupt:
         pass
+
+if __name__ == "__main__":
+    run()
